a1adcd869c320e296e8ac18d1012ec79c29701fe
Sofia Iakubauskas <Sofaakubauskas@yandex.ru>
2025-12-21 21:53:50 +0300

    add protected

--- project_name/main.py
+++ project_name/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response, Depends
 from pydantic import BaseModel
 from authx import AuthX, AuthXConfig
 
@@ -17,12 +17,13 @@ class UserLoginSchema(BaseModel):
 	password: str
 
 @app.post("/login")
-def login(creds: UserLoginSchema):
+def login(creds: UserLoginSchema, response: Response):
 	if creds.username == "test" and creds.password == "test":
 		token = security.create_access_token(uid="12345")
+		response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
 		return {"access_token": token} 
 	raise HTTPException(status_code=401, detail="Incorrect username or password")
 
-@app.get("/protected")
+@app.get("/protected", dependencies=[Depends(security.access_token_required)])
 def protected():
-	...
+	return {"data": "TOP SECRET"}
